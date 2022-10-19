5bf2de1c527f315b1670f43b92652b5072e8a00b
Pierre BELLEGUEULE <pierreb@istari.vision>
2022-10-19 09:57:44 +0200

    add workshop contracts

---
+++ sc-nft-marketplace/esdt-nft-marketplace/wasm/src/lib.rs
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    esdt_nft_marketplace
+    (
+        auctionToken
+        bid
+        buySft
+        claimTokens
+        endAuction
+        getClaimableAmount
+        getFullAuctionData
+        getLastValidAuctionId
+        getMarketplaceCutPercentage
+        isPaused
+        pause
+        setCutPercentage
+        unpause
+        withdraw
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
